b811f2f898727a70c38b283b547ad9b3c488c99c
Robben1972 <bozorovshahob27@gmail.com>
2026-01-11 11:48:29 +0500

    allowed hosts

--- kiosk/settings.py
+++ kiosk/settings.py
@@ -132,6 +132,10 @@ STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://localhost:3000",
+    "https://ezguniyatkiosk.uz"
 ]
 
 
