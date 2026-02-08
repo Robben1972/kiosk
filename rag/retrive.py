4b0d23e55495a6b817b8ce843d1ccb9bd47b2e48
Robben1972 <bozorovshahob27@gmail.com>
2026-02-08 18:30:32 +0500

    rag path

--- rag/retrive.py
+++ rag/retrive.py
@@ -6,7 +6,7 @@ from environs import Env
 from django.conf import settings
 from pathlib import Path
 
-EMB_PATH = Path(settings.BASE_DIR) / "embeddings.pkl"
+EMB_PATH = "/root/diagno_kiosk/kiosk/rag/ragembeddings.pkl"
 
 env = Env()
 env.read_env()
